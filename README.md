c0fd15841abdce46bd1306ea5e541c391f649ac7
Antonio Barrera <antbarrera@gmail.com>
2023-09-10 21:25:40 -0400

    initial commit, script to build modules and theme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Aegir Support
+
+Created to keep track of and download appropriate code for Aegir standalone.
+
+Mostly comes from repositories at:
+
+https://github.com/omega8cc
+
---
+++ build_host.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git clone -b feature/3.0.x-eldir https://github.com/omega8cc/eldir.git eldir
+git clone -b feature/3.1.x-hosting https://github.com/omega8cc/hosting.git hosting
+git clone -b 7.x-3.x https://github.com/omega8cc/aegir_objects.git aegir_objects
+git clone -b 7.x-3.x https://github.com/omega8cc/hosting_civicrm.git hosting_civicrm
+git clone -b 7.x-3.x https://github.com/omega8cc/hosting_deploy.git hosting_deploy
+git clone -b 7.x-3.x https://github.com/omega8cc/hosting_git.git hosting_git
+git clone -b 7.x-3.x https://github.com/omega8cc/hosting_le.git hosting_le
+git clone -b 7.x-3.x https://github.com/omega8cc/hosting_remote_import.git hosting_remote_import
+git clone -b 7.x-3.x https://github.com/omega8cc/hosting_site_backup_manager.git hosting_site_backup_manager
+git clone -b 7.x-3.x https://github.com/omega8cc/hosting_tasks_extra.git hosting_tasks_extra
